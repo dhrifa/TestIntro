146c1a748ee2ae54bb5bec2f6aa0bf37f7ae68d8
dhrifa <dhrifa.kherroubi@mobileconsultingsolutions.us>
2022-12-09 22:22:03 -0500

    first class test

--- app/src/test/java/com/example/testintro/TaxTest.kt
+++ app/src/test/java/com/example/testintro/TaxTest.kt
@@ -1,16 +1,35 @@
 package com.example.testintro
 
-import org.junit.Assert.*
+//import org.junit.Assert.*
 
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class TaxTest {
 
-    @Test
-    fun calculateTax() {
+    private lateinit var tax: Tax
+
+    @Before
+    fun setup() {
+        tax = Tax()
+    }
+
+    @After
+    fun tearDown(){
+
     }
 
     @Test
-    fun calculateIncome() {
+    fun `test calculateTax`() {
+        val netTax = tax.calculateTax(100.0, 0.1)
+        assertThat(netTax).isEqualTo( 10.0)
+    }
+
+    @Test
+    fun calculateIncomeTest() {
+        val netIncome = tax.calculateIncome(100.0, 0.1)
+        assertThat(netIncome).isEqualTo(90)
     }
 }
